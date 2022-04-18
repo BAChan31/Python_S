462256ad3a08e78d02b5b1eb2c65a01a989d3197
SeungChan Baek <scfreedom31@gmail.com>
2022-04-18 23:18:27 +0900

    First Try

---
+++ SelectionSort.py
@@ -0,0 +1,11 @@
+def Selectionsort(arr) :
+    for example_i in range(len(arr)-1):
+        min_index = example_i
+        for example_j in range(example_i +1,len(arr)):
+            if arr[example_j] < arr[min_index]:
+                min_index= example_j
+        arr[example_i], arr[min_index] = arr[min_index], arr[example_i]
+
+arr_i=[1,24,66,2,4]
+Selectionsort(arr_i)
+print(arr_i)
